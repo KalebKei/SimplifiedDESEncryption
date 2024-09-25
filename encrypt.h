a9ba393aa50b7ec426e9d140546661f9146cba6a
KalebKei <kk471921@ohio.edu>
2024-09-25 11:17:27 -0400

    Now with more documentation!

--- encrypt.h
+++ encrypt.h
@@ -65,6 +65,13 @@ std::string ReadFile(const char* file);
  */
 void EncryptByte(unsigned char input, u_int16_t key);
 
+/**
+ * @brief The Feistel function implemented. Takes in an 8 bit input alongside a 10 bit key. Yes, it's spelled wrong it's a gimmick at this point
+ * 
+ * @param input The 8 bit character to be encrypted
+ * @param key The 10 bit key used for encryption
+ * @return u_int16_t The encrypted input
+ */
 u_int16_t Feistal(unsigned char input, u_int16_t key);
 
 
@@ -78,36 +85,116 @@ u_int16_t Feistal(unsigned char input, u_int16_t key);
  */
 void ToBinaryArr(int binary[], u_int16_t val, int size);
 
+/**
+ * @brief Converts a binary array (integer array containing only 1's and 0's) to a unsigned 16 bit integer
+ * 
+ * @param binary The binary representation of a integer up to 16 bits
+ * @param size The size of the binary array up to 16 bits
+ * @return u_int16_t The integer representation of the inputted binary array
+ */
 u_int16_t ToInt(int binary[], int size);
 
+/**
+ * @brief Implements a left bit shift with wrap around
+ * 
+ * @param val The binary array representation of what we want to shift
+ * @param size The size of the array
+ */
 void LeftShift(int val[], int size);
 
+/**
+ * @brief Splits an array in half
+ * 
+ * @param binary The array to be split
+ * @param left The output left side of the array
+ * @param right The output right side of the array
+ * @param size The size of the full binary array
+ */
 void SplitArr(int binary[], int left[], int right[], int size);
 
+/**
+ * @brief Combines to arrays together
+ * 
+ * @param full The output array
+ * @param left The input left side of the soon to be output
+ * @param right The input right side of the soon to be output
+ * @param full_size The size of the output array
+ */
 void CombineArrs(int full[], int left[], int right[], int full_size);
 
+/**
+ * @brief Swaps the front half of an array with the back half
+ * 
+ * @param binary The array to be swapped
+ * @param size The size of the array
+ */
 void Swap(int binary[], int size);
 
 /*** Permutations ***/
 
 /* Actually all of the work */
 
+/**
+ * @brief A function that performs required bit permutations on values up to size 16 bits. Not intended for direct use
+ * 
+ * @param val The value to be permutated
+ * @param input_size The size of the binary representation of the input
+ * @param permutation_size The size of the permutation output
+ * @param permutation Uses global variables defined at the top of this header file to define the permutation to be used. 
+ * @return u_int16_t The permutated output
+ */
 u_int16_t Permutation(u_int16_t val, int input_size, int permutation_size, int permutation[]);
 
 /* Encryption */
 
+/**
+ * @brief Ease of use IP permutation
+ * 
+ * @param ch 8 bit character to be permutated
+ * @return unsigned char The permutated character
+ */
 unsigned char PermIP(unsigned char ch);
 
+/**
+ * @brief Ease of use IP-1 permutation
+ * 
+ * @param ch 8 bit character to be permutated
+ * @return unsigned char The permutated character
+ */
 unsigned char PermIPn(unsigned char ch);
 
 /* Key Gen */
 
+/**
+ * @brief Ease of use P10 permutation
+ * 
+ * @param val The 10 bit character to be permutated
+ * @return u_int16_t The permutated character
+ */
 u_int16_t PermP10(u_int16_t val);
 
+/**
+ * @brief Ease of use P8 permutation
+ * 
+ * @param val The 10 bit character input to be permutated
+ * @return u_int16_t The 8 bit permuted character
+ */
 u_int16_t PermP8(u_int16_t val);
 
 /* Feistal Function */
 
+/**
+ * @brief Ease of use expansion permutation
+ * 
+ * @param val The 4 bit character input to be permutated
+ * @return u_int16_t The 8 bit permutated character
+ */
 u_int16_t Expansion(u_int16_t val);
 
+/**
+ * @brief Ease of use P4 permutation
+ * 
+ * @param val The character to be permutated
+ * @return u_int16_t The permutated character
+ */
 u_int16_t PermP4(u_int16_t val);
